5fcb076c069373036b44424c82d73e3b0bb63f64
gilias24-meet <ashelgal@gmail.com>
2024-07-17 14:09:40 +0300

    new

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__,
 template_folder='templates',
 static_folder='static')
+
 @app .route('/home')
 def home():
-	return render_template("home.html")
+	return render_template("hello.html")
+
+
+@app .route('/fortune')
+def fortune():
+	fortunes = ["Death", "wealth", "happiness", "joy", "sadness", "love", "hate", "anger", "boredom", "stress"]
+	index_f = random.randint(0, 9)
+	f = fortunes(index_f)
+
+
+
+	return render_template("fortune.html")
+
+if __name__ == '__main__':
+	app.run(debug=True)
 	
