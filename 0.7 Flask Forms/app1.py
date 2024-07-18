534608f5f267d4b8e99236dc7da5ed3b228497dc
gilias24-meet <ashelgal@gmail.com>
2024-07-18 08:45:50 +0300

    new

--- 0.7 Flask Forms/app1.py
+++ 0.7 Flask Forms/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, method
 import random
 app = Flask(__name__,
 template_folder='templates',
@@ -6,11 +6,17 @@ static_folder='static')
 
 @app .route('/home')
 def home():
-	return render_template("hello.html")
+	if request.method == 'GET':
+		return render_template("hello.html")
+	else:
+		birth = request.form['birth month']
+		return redirect(url_for('fortune',
+            b = birth))
 
 
-@app .route('/fortune')
-def fortune():
+
+@app .route('/fortune/<string:b>')
+def fortune(b):
 	fortunes = ["You  will never submit the lab on time", "you will only have du until the end of the week", "you will lose your water bottle", "you will not be on time for complementary", "you will eat IASA food forever", "Your computer will die,", "you will win the entrp contest", "you wiil have no free time,", "your pet will die,", "tomorrow you will discover something shocking"]
 	i = random.randint(0, 9)
 	f = fortunes[i]
@@ -18,5 +24,5 @@ def fortune():
 	return render_template("fortune.html", f = f)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=3000)
 	
