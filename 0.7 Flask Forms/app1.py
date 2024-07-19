c02080405eb42a049958089982aaa890a4d73fbb
gilias24-meet <ashelgal@gmail.com>
2024-07-19 18:31:08 +0300

    new

--- 0.7 Flask Forms/app1.py
+++ 0.7 Flask Forms/app1.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
-from flask import session as login_session
-
+from flask import Flask, render_template, request, url_for, redirect
+import random
 app = Flask(__name__,
 template_folder='templates',
 static_folder='static')
@@ -12,22 +11,22 @@ def login():
        username = request.form['birth']
    return render_template("login.html")
 
-# @app.route('/fortune/<string:b>')
-# def fortune(b):
-# 	fortunes = ["You  will never submit the lab on time", "you will only have du until the end of the week", "you will lose your water bottle", "you will not be on time for complementary", "you will eat IASA food forever", "Your computer will die,", "you will win the entrp contest", "you wiil have no free time,", "your pet will die,", "tomorrow you will discover something shocking"]
-# 	l =len(b)
+@app.route('/fortune/<string:b>')
+def fortune(b):
+	fortunes = ["You  will never submit the lab on time", "you will only have du until the end of the week", "you will lose your water bottle", "you will not be on time for complementary", "you will eat IASA food forever", "Your computer will die,", "you will win the entrp contest", "you wiil have no free time,", "your pet will die,", "tomorrow you will discover something shocking"]
+	l =len(b)
 
 
-# 	if len(b) <= 9:
-# 		f = fortunes[l]
+	if len(b) <= 9:
+		f = fortunes[l]
 		
-# 	else:
-# 		f = "You  will never submit the lab on time"
+	else:
+		f = "You  will never submit the lab on time"
 		
-# 	return render_template("fortune.html", f = f)
+	return render_template("fortune.html", f = f)
 
-# if __name__ == '__main__':
-# 	app.run(debug = True, port='3000')
+if __name__ == '__main__':
+	app.run(debug = True, port='3000')
 
 
 
