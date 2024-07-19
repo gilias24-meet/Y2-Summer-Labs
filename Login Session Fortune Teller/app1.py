ab9d44ffe00982dc33cd7e0fde5d3d73e62e9b18
gilias24-meet <ashelgal@gmail.com>
2024-07-19 16:23:26 +0300

    new

---
+++ Login Session Fortune Teller/app1.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, url_for, redirect
+import random
+app = Flask(__name__,
+template_folder='templates',
+static_folder='static')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	if request.method == 'GET':
+		return render_template("hello.html")
+	else:
+		birth = request.form['birth']
+	
+		return redirect(url_for('fortune',
+            b = birth))
+
+
+
+
+@app.route('/fortune/<string:b>')
+def fortune(b):
+	fortunes = ["You  will never submit the lab on time", "you will only have du until the end of the week", "you will lose your water bottle", "you will not be on time for complementary", "you will eat IASA food forever", "Your computer will die,", "you will win the entrp contest", "you wiil have no free time,", "your pet will die,", "tomorrow you will discover something shocking"]
+	l =len(b)
+
+
+	if len(b) <= 9:
+		f = fortunes[l]
+		
+	else:
+		f = "You  will never submit the lab on time"
+		
+	return render_template("fortune.html", f = f)
+
+if __name__ == '__main__':
+	app.run(debug = True, port='3000')
+
+
+
+	
